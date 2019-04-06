0160a5cb11a652028a382561303f9bee9f5e0090
Krzysztof <swiatlyk@gmail.com>
2019-04-06 19:08:04 +0200

    Initial commit

---
+++ PasswdGener/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace PasswdGener
+{
+    public static class PasswordGenerator
+    {
+        /// <summary>
+        /// Generates a random password based on the rules passed in the parameters
+        /// </summary>
+        /// string includeLowercase>Bool to say if lowercase are required</param>
+        /// <stringincludeUppercase>Bool to say if uppercase are required</param>
+        /// <param name="includeNumeric">Bool to say if numerics are required</param>
+        /// <param name="includeSpecial">Bool to say if special characters are required</param>
+        /// <param name="includeSpaces">Bool to say if spaces are required</param>
+        /// <param name="lengthOfPassword">Length of password required. Should be between 8 and 128</param>
+        /// <returns></returns>
+        public static string GeneratePassword(bool includeLowercase, bool includeUppercase, bool includeNumeric, bool includeSpecial, bool includeSpaces, int lengthOfPassword)
+        {
+            const int MAXIMUM_IDENTICAL_CONSECUTIVE_CHARS = 2;
+            const string LOWERCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+            const string UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string NUMERIC_CHARACTERS = "0123456789";
+            const string SPECIAL_CHARACTERS = @"!#$%&*@\";
+            const string SPACE_CHARACTER = " ";
+            const int PASSWORD_LENGTH_MIN = 8;
+            const int PASSWORD_LENGTH_MAX = 128;
+
+            if (lengthOfPassword < PASSWORD_LENGTH_MIN || lengthOfPassword > PASSWORD_LENGTH_MAX)
+            {
+                return "Password length must be between 8 and 128.";
+            }
+
+            string characterSet = "";
+
+            if (includeLowercase)
+            {
+                characterSet += LOWERCASE_CHARACTERS;
+            }
+
+            if (includeUppercase)
+            {
+                characterSet += UPPERCASE_CHARACTERS;
+            }
+
+            if (includeNumeric)
+            {
+                characterSet += NUMERIC_CHARACTERS;
+            }
+
+            if (includeSpecial)
+            {
+                characterSet += SPECIAL_CHARACTERS;
+            }
+
+            if (includeSpaces)
+            {
+                characterSet += SPACE_CHARACTER;
+            }
+
+            return "not random";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<-------          Password Generator          ------->");
+            Console.WriteLine("<-------          Generates password          ------->");
+            Console.WriteLine("<-------Generate password from 8 to 128 chars ------->");
+            Console.ReadKey();
+        }
+    }
+}
